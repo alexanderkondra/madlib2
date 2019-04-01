c67e43594d46d4b3adc35b3e84844fbfae6b5040
Alexander Kondra and Mustafa Ghasemi <mustafa@>
2019-04-01 11:40:03 -0700

    replace forEach looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event){
-    var aNumberInput = $("input#number").val();
-    var aLocationInput = $("input#location").val();
-    var aAdjectiveInput = $("input#adjective").val();
-    var aPluralAnimalInput = $("input#plural-animal").val();
-    var aVerbInput = $("input#verb").val();
+    var blanks =["number","location","adjective","plural-animal"];
+    // var aNumberInput = $("input#number").val();
+    // var aLocationInput = $("input#location").val();
+    // var aAdjectiveInput = $("input#adjective").val();
+    // var aPluralAnimalInput = $("input#plural-animal").val();
+    // var aVerbInput = $("input#verb").val();
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    })
+    // $(".number").text(aNumberInput);
+    // $(".location").text(aLocationInput);
+    // $(".adjective").text(aAdjectiveInput);
+    // $(".plural-animal").text(aPluralAnimalInput);
+    // $(".verb").text(aVerbInput);
 
-    $(".number").text(aNumberInput);
-    $(".location").text(aLocationInput);
-    $(".adjective").text(aAdjectiveInput);
-    $(".plural-animal").text(aPluralAnimalInput);
-    $(".verb").text(aVerbInput);
-
-    $(story).show();
+    $("#story").show();
     event.preventDefault();
   });
 });
